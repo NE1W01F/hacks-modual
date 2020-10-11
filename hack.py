21045add5df008fc3219dfa887d3fd8d6adc251c
Wolf <36646784+NE1W01F@users.noreply.github.com>
2020-10-11 04:20:23 +0000

    Add files via upload

--- hack.py
+++ hack.py
@@ -14,11 +14,36 @@ def register(cb):
 class hacksMod(loader.Module):
     """Fack FBI Hacking Script and Other Cool Scripts"""
     strings = {"name": "Hacks"}
+
+    love_jokes = [
+        "Baby are you a motherboard?, Cause I'd RAM you all night long.",
+        "You make me want to calibrate my joystick without the latest drivers",
+        "You want to learn about computers huh, you've already passed the first lesson. Turning Me On",
+        "You defragment my life",
+        "Girl, are you Wi-Fi? Cuz im feeling the connection!",
+        "Would you like to enjoy my laptop, I promise I don't have any viruses...",
+        "Girl, you got software? I've got hardware. Together, we can liveware ever we want.",
+        "Baby, there is no part of my body that is Micro or Soft",
+        "I'm definitely in the range of your hotspot. How about you let me connect and get full access.",
+        "You PnP? I wanna RAM this RAW Hard Disk up your Megahertz'd Computer. Nothing PC bout it.",
+        "There is no cache, lets go straight to the hard drive.",
+        "If I were an assembly language, I'd jump to your address, shift right a bit, push it in, pop it out, load a byte into your accumulator, then jump if you're negative.",
+        "You can put a Trojan on my Hard Drive anytime",
+        "If I were an operating system, your process would be real-time priority.",
+        "Nobody turns me on from a cold boot like you.",
+        "Baby, you overclock my processor.",
+        "Baby, you make my floppy disk turn into a hard drive",
+        "You still use Internet Explorer, you must like it nice and slow",
+        "Are you a computer whiz? it seems you know how to turn my software to hardware.",
+        "Baby you're so cute you made my page 404.",
+        "Hey baby, I'm a power source, and you're the kind of resistor i'd like to deliver my load to.",
+        "Baby, let's configure our hard drives in master and slave position."
+    ]
     
     def __init__(self):
         pass
 
-    
+
     async def timecmd(self, message):
         """Gets the Current Time"""
         data = datetime.now()
@@ -37,6 +62,10 @@ class hacksMod(loader.Module):
         ret += "<b>SHA256:</b> <code>{}</code>".format(hash)
         await utils.answer(message, ret)
 
+
+    async def lovecmd(self, message):
+        rad = randint(0, len(self.love_jokes))
+        await utils.answer(message, self.love_jokes[rad])
 
     async def hackscmd(self, message):
         """Hacks the FBI when you type .hacks"""
