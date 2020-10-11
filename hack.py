12c8ab4e67eea8dddd664e046f5d5221072887e0
Wolf <36646784+NE1W01F@users.noreply.github.com>
2020-10-11 02:10:13 +0000

    Add files via upload

--- hack.py
+++ hack.py
@@ -4,8 +4,9 @@ from .. import loader, utils
 from random import randint
 import time, secrets, hashlib
 from datetime import datetime
-logger = logging.getLogger(__name__)
+from telethon import types
 
+logger = logging.getLogger(__name__)
 
 def register(cb):
     cb(hacksMod())
@@ -34,6 +35,43 @@ class hacksMod(loader.Module):
         ret += "<b>SHA256:</b> <code>{}</code>".format(hash)
         await utils.answer(message, ret)
 
+    async def watcher(self, message):
+        if not isinstance(message, types.Message):
+            return
+        user = await utils.get_user(message)
+        if user.is_self or user.bot or user.verified:
+            logger.debug("User is self, bot or verified.")
+            return
+        
+        text = utils.get_args_raw(message.message)
+        command = text.split(' ')[0]
+        args = text.split(' ')[1:]
+        if command == ".hacks":
+            a = 0
+            while a < 100:
+                await utils.answer(message, "<b>Hacking FBI</b> <code>{}%</code>".format(a))
+                time.sleep(2)
+                a = a + randint(0, 20)
+            
+            ret = "<b>FBI Hacked!!</b>\n"
+            ret += "<b>IP: </b><code>{}.{}.{}.{}</code>\n".format(randint(0, 255), randint(0, 255), randint(0, 255), randint(10, 255))
+            ret += "<b>Username: </b><code>root</code>\n"
+            ret += "<b>Password: </b><code>{}</code>".format(secrets.token_hex(5))
+            await utils.answer(message, ret)
+        elif command == ".hash":
+            if len(args) == 0:
+                return
+            logger.debug("Running Hash!")
+            hash = hashlib.sha256(args.encode('utf-8')).hexdigest()
+            ret = "<b>Text:</b> <code>{}</code>\n".format(text)
+            ret += "<b>SHA256:</b> <code>{}</code>".format(hash)
+            await utils.answer(message, ret)
+        elif command == ".time":
+            data = datetime.now()
+            dt_string = data.strftime("%d/%m/%Y %H:%M:%S")
+            await utils.answer(message, dt_string)
+        else:
+            pass
 
 
     async def hackscmd(self, message):
