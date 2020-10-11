214396e83cbc060a49a22fba763222c903fc6f53
Wolf <36646784+NE1W01F@users.noreply.github.com>
2020-10-11 01:02:00 +0000

    Add files via upload

--- hack.py
+++ hack.py
@@ -2,33 +2,43 @@ import logging
 
 from .. import loader, utils
 from random import randint
-import time, secrets
+import time, secrets, hashlib
 
 logger = logging.getLogger(__name__)
 
 
 def register(cb):
-    cb(HacksMod())
+    cb(hacksMod())
 
 
-class HacksMod(loader.Module):
-
+class hacksMod(loader.Module):
     """Fack FBI Hacking Script"""
+    
     def __init__(self):
         pass
+    
+    async def hashcmd(self, message):
+        """Makes a SHA256 hash when you type .hash"""
+        text = utils.get_args_raw(message.message)
+        if len(text) == 0:
+            return
+        logger.debug("Running Hash!")
+        hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
+        ret = "<b>Text:</b> <code>{}</code>\n".format(text)
+        ret += "<b>SHA256:</b> <code>{}</code>".format(hash)
+        await utils.answer(message, ret)
 
     async def hackscmd(self, message):
         """Hacks the FBI when you type .hacks"""
         logger.debug("Running Hacks!")
         a = 0
         while a < 100:
-            msg = "<b>Hacking FBI</b> <code>{}%</code>".format(a)
-            await utils.answer(message, msg)
-            time.sleep(3)
+            await utils.answer(message, "<b>Hacking FBI</b> <code>{}%</code>".format(a))
+            time.sleep(2)
             a = a + randint(0, 20)
         
         ret = "<b>FBI Hacked!!</b>\n"
-        ret += "<b>IP:</b> <code>{}.{}.{}.{}</code>\n".format(randint(0, 255), randint(0, 255), randint(0, 255), randint(10, 255))
-        ret += "<b>Username:</b> <code>root</code>\n"
-        ret += "<b>Password:</b> <code>{}</code>".format(secrets.token_hex(5))
+        ret += "<b>IP: <\b><code>{}.{}.{}.{}</code>\n".format(randint(0, 255), randint(0, 255), randint(0, 255), randint(10, 255))
+        ret += "<b>Username:</b><code>root</code>\n"
+        ret += "<b>Password: </b><code>{}</code>".format(secrets.token_hex(15))
         await utils.answer(message, ret)
